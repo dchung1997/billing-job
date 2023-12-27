5fbec4c6d8e6956900a1a73359f157f26d96181a
David <davidmchung123@gmail.com>
2023-12-27 14:18:08 -0600

    Added file preperation for single step of billing job.

--- src/main/java/com/example/billingjob/BillingJobConfiguration.java
+++ src/main/java/com/example/billingjob/BillingJobConfiguration.java
@@ -1,8 +1,29 @@
 package com.example.billingjob;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.JdbcTransactionManager;
 
 @Configuration
 public class BillingJobConfiguration {
     // TODO;
+    @Bean
+    public Job job(JobRepository jobRepository, Step step1) {
+        return new JobBuilder("BillingJob", jobRepository)
+                .start(step1)
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository JobRepository, JdbcTransactionManager transcationManager) {
+        return new StepBuilder("filePreparation", JobRepository)
+                .tasklet(new FilePreparationTasklet(), transcationManager)
+                .build();
+    }
 }
+
--- src/test/java/com/example/billingjob/BillingJobApplicationTests.java
+++ src/test/java/com/example/billingjob/BillingJobApplicationTests.java
@@ -1,10 +1,53 @@
 package com.example.billingjob;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBatchTest
 @SpringBootTest
 class BillingJobApplicationTests {
+	
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Autowired
+	private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+	@BeforeEach
+	public void setUp() {
+		this.jobRepositoryTestUtils.removeJobExecutions();
+	}	
+
+
+	@Test 
+	void testJobExecution() throws Exception {
+		// Given
+		JobParameters jobParameters = new JobParametersBuilder()
+										.addString("input.file", "src/main/resources/billing-2023-01.csv")
+										.toJobParameters();
+
+		// When
+		JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
+		
+
+		// Then
+		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+		Assertions.assertTrue(Files.exists(Paths.get("staging", "billing-2023-01.csv")));
+	}
 
 	@Test
 	void contextLoads() {
